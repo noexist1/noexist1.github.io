b3342b53878658798151821db85fc44b3838e900
noexist1 <a.vorontsova1@g.nsu.ru>
2023-12-28 14:56:07 +0700

    demo2.0

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -20,8 +20,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'noexist1', // Usually your GitHub org/user name.
+  projectName: 'my-website', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
