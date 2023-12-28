7e27f12a28aab7e3b017e226ff4ebcd852e04e8e
noexist1 <a.vorontsova1@g.nsu.ru>
2023-12-28 14:51:03 +0700

    url

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'src/img/logo.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://noexist1.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/my-website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
